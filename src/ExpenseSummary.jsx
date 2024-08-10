a99e0381b72f527211e444fc2a50c848e3a24316
Dattu Naik G <66054141+Dattunaik5522@users.noreply.github.com>
2024-08-10 04:47:12 +0000

    Initial commit

---
+++ src/ExpenseSummary.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./css/ExpenseList.module.scss";
+
+function ExpenseSummary({ expenses }) {
+  const totalExpenses = expenses.reduce(
+    (acc, expense) => acc + expense.amount,
+    0
+  );
+
+  return (
+    <div className={styles.mainContainer}>
+      <h2>Summary</h2>
+      <p>Total Expenses: Rs {totalExpenses.toFixed(2)}</p>
+      {/* Additional filters and summaries can be added here */}
+    </div>
+  );
+}
+
+export default ExpenseSummary;
